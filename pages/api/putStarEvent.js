bfd97c9c6f784d31e8e2f1aa6651c1fab5413a00
gnikhil98 <153263393+gnikhil98@users.noreply.github.com>
2024-11-22 10:12:29 +0530

    env issue

--- pages/api/putStarEvent.js
+++ pages/api/putStarEvent.js
@@ -7,9 +7,9 @@ const AWS = require('aws-sdk');
 // });
 
 const eventBridge = new AWS.EventBridge({
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  region: process.env.REGION,
+  // accessKeyId: process.env.ACCESS_KEY_ID,
+  // secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: 'us-west-1',
 });
 
 export default async function handler(req, res) {
